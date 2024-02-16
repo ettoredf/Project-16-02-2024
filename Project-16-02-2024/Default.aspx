5dc1c8089a737d515c5dfdbd88aac615aa7b5658
ettoredf <147718125+ettoredf@users.noreply.github.com>
2024-02-16 16:35:17 +0100

    ok

---
+++ Project-16-02-2024/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Project_16_02_2024._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+       
+    </main>
+
+</asp:Content>
---
+++ Project-16-02-2024/Default.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Questo codice è stato generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Project_16_02_2024
+{
+
+
+    public partial class _Default
+    {
+    }
+}
